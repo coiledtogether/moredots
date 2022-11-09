1a9433b368a54df32e2113b4c66d6da1da508211
virtualgirl <114882772+coiledtogether@users.noreply.github.com>
2022-11-09 06:15:01 -0500

    autostart

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -7,3 +7,23 @@ wpg -s lavender.png &&
 
 kitty --hold -e neofetch &
 wmname LG3D &
+
+while true; do
+	
+	memory=$(free -h | awk '(NR==2){ print $3 }')
+
+	speaker=$(amixer get -c 2 Master | awk -F'[][]' 'END{print $2 }')
+
+	microphone=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
+	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
+	
+	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
+
+	battery=$(cat /sys/class/power_supply/BAT0/capacity)
+
+	clock=$(date +'[%m.%d.%H.%M.%S.%N]')	
+
+	xsetroot -name " T::$temperature | M: $memory | S::$speaker | $clock " &
+	
+	wait
+done &
