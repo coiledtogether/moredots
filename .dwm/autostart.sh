2277d2583f234ba8375c310dfaccffe7434e7acf
coiledtogether <114882772+coiledtogether@users.noreply.github.com>
2022-11-14 18:41:58 -0500

    update configs

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,26 +6,28 @@
 picom --experimental-backends &
 wpg -s lavender.png &&
 
-kitty --hold -e neofetch &
+#kitty --hold -e neofetch &
 wmname LG3D &
+slstatus &
+xautolock -time 20 -locker "slock" -detectsleep
 
-while true; do
+#while true; do
 	
-	memory=$(free -h | awk '(NR==2){ print $3 }')
+#	memory=$(free -h | awk '(NR==2){ print $3 }')
 
-	speaker=$(amixer get -c 1 Master | awk -F'[][]' 'END{print $2 }')
+#	speaker=$(amixer get -c 1 Master | awk -F'[][]' 'END{print $2 }')
 
-	clock=$(date +'[%m.%d.%H.%M.%S.%N]')	
+#	clock=$(date +'[%m.%d.%H.%M.%S]')	
 
-	#temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)	
-	temperature=$(sensors | grep CPUTIN | awk '{print substr($2, 2, length($2))}')
+#	#temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)	
+#	temperature=$(sensors | grep CPUTIN | awk '{print substr($2, 2, length($2))}')
 
-	#microphone=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
-	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
+#	#microphone=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
+#	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
 	
-	#battery=$(cat /sys/class/power_supply/BAT0/capacity)
+#	#battery=$(cat /sys/class/power_supply/BAT0/capacity)
 
-	xsetroot -name " T::$temperature | M::$memory | V::$speaker | $clock " &
+#	xsetroot -name " T::$temperature  |  M::$memory  |  V::$speaker  |  $clock " &
 	
-	wait
-done &
+#	wait
+#done &
--- .dwm/config.h
+++ .dwm/config.h
@@ -4,14 +4,14 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 32;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 18;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 48;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 48;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Gohu GohuFont:size=8" };
+static const char dmenufont[]       = "Gohu GohuFont:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -72,9 +72,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -2 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -2 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
